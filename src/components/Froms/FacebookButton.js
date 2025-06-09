7b8e63e006c2d66dd4eea4efabe6ea101020704c
Ebramrasmy <ebramrasmy7@gmail.com>
2025-06-09 21:29:58 +0300

    add FacebookButton.js to froms folder

---
+++ src/components/Froms/FacebookButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaFacebook } from "react-icons/fa";
+
+const FacebookButton = () => {
+  return (
+    <button
+      type="button"
+      className="w-full flex items-center justify-center gap-3 py-2 px-4 rounded-lg bg-[#3b5998] hover:bg-[#2d4373] text-white font-medium text-sm transition"
+    >
+      <FaFacebook size={20} />
+      Continue with Facebook
+    </button>
+  );
+};
+
+export default FacebookButton;
