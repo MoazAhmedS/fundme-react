34c56ffcc84417cd5a174f67ef85b3f045a8c1f7
mahmoudOraby <mahmoud.eelu.2020@gmail.com>
2025-06-09 23:06:36 +0300

    add mossion section in About US

--- src/components/pages/ AboutUs.jsx
+++ src/components/pages/ AboutUs.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import HeroSection from '../about/HeroSection';
 import Navbar from '../NavigationComponents/Navbar';
 import Footer from '../NavigationComponents/Footer';
+import MissionSection from '../about/MissionSection';
 
 const AboutUs = () => {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
-     <Navbar/>
+      <Navbar/>
       <HeroSection />
+      <MissionSection/>
       <Footer/>
     </div>
   );
