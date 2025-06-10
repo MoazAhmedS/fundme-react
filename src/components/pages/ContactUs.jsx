350afae1b676192b564139718c52f35a78f21041
mahmoudOraby <mahmoud.eelu.2020@gmail.com>
2025-06-10 15:44:28 +0300

    add contact method section in Contact Us

--- src/components/pages/ContactUs.jsx
+++ src/components/pages/ContactUs.jsx
@@ -1,7 +1,7 @@
 import Navbar from '../NavigationComponents/Navbar';
 import Footer from '../NavigationComponents/Footer';
 import ContactHeader from '../Contact/ContactHeader';
-
+import ContactMethods from '../Contact/ContactMethods'
 
 const ContactUs = () => {
   return (
@@ -9,8 +9,11 @@ const ContactUs = () => {
     <Navbar/>
     <div className="min-h-screen bg-gray-900 text-white">
       <ContactHeader />
-      
-     
+      <section className="py-16 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto">
+          <ContactMethods />
+        </div>
+      </section>
     </div>
     <Footer/>
     </>
