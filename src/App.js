c0beef808553aaab0bcb7fd17cfc27b370c69fce
gergeskeliny <gergeskeliny60@gmail.com>
2025-06-11 00:52:17 +0300

    put allProjects in pages folder

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import AllProjects from './AllProjects';
 function App() {
   return (
     <div className="App">
-      <AllProjects />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
