6b115e7d69af39189b3c51a4876e821e82e03ede
Ebramrasmy <ebramrasmy7@gmail.com>
2025-06-11 02:33:01 +0300

    Edit in components FormWarpper ,showHidePassword and SubmitButton

--- src/components/Froms/ShowHidePassword.js
+++ src/components/Froms/ShowHidePassword.js
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-
-const ShowHidePassword = ({ id, value, onChange }) => {
+import { FaLock } from "react-icons/fa";
+const ShowHidePassword = ({
+  id,
+  name,
+  value,
+  onChange,
+  onBlur,
+  className = "",
+  placeholder = "",
+}) => {
   const [show, setShow] = useState(false);
 
   return (
     <div className="relative">
+      <FaLock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
       <input
         id={id}
+        name={name}
         type={show ? "text" : "password"}
         value={value}
         onChange={onChange}
-        className="w-full px-3 py-2 rounded-lg bg-[#1e1e3f] text-white border border-gray-600 pr-10"
+        onBlur={onBlur}
+        placeholder={placeholder} 
+        className={`w-full px-3 py-2 rounded-lg bg-[#1e1e3f] text-white border pr-10 ${className}`}
       />
       <div
         className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-white"
@@ -22,5 +34,4 @@ const ShowHidePassword = ({ id, value, onChange }) => {
     </div>
   );
 };
-
 export default ShowHidePassword;
