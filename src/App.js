ad88b8b0dda5ce897513eeb4aa46c54178036bdd
MoazAhmedS <moazahmed0101@gmail.com>
2025-06-12 01:17:22 +0300

    reset password using api Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import ForgotPassword from './components/pages/ForgotPassword';
+import ResetPassword from './components/pages/ResetPassword';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ForgotPassword/>} />
+          <Route path="/reset-password/:uidb64/:token" element={<ResetPassword />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
